7e7a018eda406994d40dbecd0a899e81faf42da0
richard-xu1 <38999465+richard-xu1@users.noreply.github.com>
2018-12-08 15:35:23 -0500

    Add files via upload

--- data.py
+++ data.py
@@ -20,16 +20,42 @@ def read(file):
 	for i in range(len(datalines)):
 		dataline_list = datalines[i].split()
 		for j in range(n_features-1):
-			dataline_list[j+1] = dataline_list[j+1][2:]
+			if j >= 9:
+				dataline_list[j+1] = dataline_list[j+1][3:]
+			else:
+				dataline_list[j+1] = dataline_list[j+1][2:]
 		datarray[i,:] = dataline_list
 	return datarray
+
+def split(file, seed = 0, p_train = 0.8):
+	"""
+	Splits a given data file into a training set and a testing set. Using 80% of the data as the training set
+	and 20% of the data as the testing set. A seed can be passed as a parameter to change how the function
+	random splits the data. p_train is the percentage of data points that will be training data. 
+
+	Writes the training and testing data to new files called 'file'+ '_test' + str(seed) and 'file' + '_train' + str(seed).
+
+	@ Param: file, seed, p_train
+	@ Return: data_train, data_test
+	"""
+	np.random.seed(seed)
+	datarray, data_train, data_test = read(file), [], []
+	for i in range(len(datarray)):
+		select = np.random.binomial(1, p_train)
+		if select == 0:
+			data_test.append(datarray[i])
+		else:
+			data_train.append(datarray[i])
+	np.save((file[:-4]+ "_train" + str(seed)+".npy"), data_train)
+	np.save((file[:-4]+ "_test" + str(seed) + ".npy"), data_test)	
+	return data_train, data_test
 
 def __checkdata(file):
 	"""
 	Takes a file with data in the 'libsvm' format and returns a list where each element is a line of the data
 	This function removes all lines in the data where data for any features is missing. This function relies on the
 	first line in the data having the full number of features so that needs to be checked manually beforehand.
-	
+
 	@ Params: file
 	@ Return: datalines (missing data is removed)
 	"""
