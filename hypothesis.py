c45bf1c5c043def5b9ac9582db4676d951d55ea2
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2018-12-13 20:35:31 -0500

    index starts from 0 but size from 1 in Python

--- hypothesis.py
+++ hypothesis.py
@@ -1,29 +1,30 @@
-import numpy as np 
+import numpy as np
 import sys
 
 def generate_baseclassifiers(file, n):
 	"""
 	Given a '.npy' file with data stored in a 2D array where each sample is a row in the data,
-    this function generates a set of base classifiers of threshold functions that separate 
+    this function generates a set of base classifiers of threshold functions that separate
     the range over which each features spans into 'n' possible thresholds.
 
 	@ Param: file, n
 	@ Return: 2D array of feature x threshold
 	"""
+	import pdb; pdb.set_trace()
 	data, n = np.load(file), int(n)
-	feature_ranges,	N_feats = np.zeros((2,data.shape[1]-1)),data.shape[1]-1
+	feature_ranges,	N_feats = np.zeros((2,data.shape[1])),data.shape[1]-1
 	for j in range(N_feats):
 		for i in range(data.shape[0]):
 			if data[i,j+1] < feature_ranges[0,j]:
 				feature_ranges[0,j] = data[i,j+1]
-			elif data[i,j+1] > feature_ranges[1,j]:	
+			elif data[i,j+1] > feature_ranges[1,j]:
 				feature_ranges[1,j] = data[i,j+1]
-
-	classifiers = np.zeros((n,N_feats))
+	import pdb; pdb.set_trace()
+	classifiers = np.zeros((n,N_feats+1))
 
 	for k in range(N_feats):
 		classifiers[:,k] = np.transpose(np.linspace(feature_ranges[0,k], feature_ranges[1,k], n))
-
+	import pdb; pdb.set_trace()
 	return classifiers
 
 if __name__ == '__main__':
